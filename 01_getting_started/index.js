8b6839f3c93c842edd451f1cba371ed6f0794d78
Kevin Bervoets <kevinbervoets@Kevins-MacBook-Pro-2.local>
2018-05-30 16:32:46 +0200

    adding some routing

--- 01_getting_started/index.js
+++ 01_getting_started/index.js
@@ -1,8 +1,29 @@
 let express = require('express');
+let fs = require('fs');
+let _ = require('lodash');
+
+let users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
+    if(err) throw err
+
+    JSON.parse(data).forEach( user => {
+        user.name.full = _.startCase(`${user.name.first} ${user.name.last}`);
+        users.push(user);
+    })
+});
 
 let app = express();
 
 app.get('/', (req, res) => res.send('Hello World!!!!!!!!'));
 app.get('/hello', (req, res) => res.send(`What's up?`));
-
+app.get('/users', (req, res) => {
+    let buffer = '';
+    users.forEach( user => buffer += `<a href="/${user.username}">${user.name.full}</a><br>` );
+    res.send(buffer);
+});
+app.get('/:username', (req,res) => {
+    let username = req.params.username;
+    res.send(username);
+});
 let server = app.listen(3000, () => console.log(`Server running at http://localhost:${server.address().port}`));
