8abd93ea92810506f1b7598a4f128abeef408faa
Kevin Bervoets <kevinbervoets@Kevins-MacBook-Pro-2.local>
2018-05-31 12:27:20 +0200

    adding template engine and serving static files with Express

--- 01_getting_started/index.js
+++ 01_getting_started/index.js
@@ -1,13 +1,14 @@
-let express = require('express');
-let fs = require('fs');
-let _ = require('lodash');
+const express = require('express');
+const fs = require('fs');
+const _ = require('lodash');
+const hbs = require('hbs');
 
 let users = [];
 
 fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
-    if(err) throw err
+    if(err) throw err;
 
-    JSON.parse(data).forEach( user => {
+    JSON.parse(data).forEach(user => {
         user.name.full = _.startCase(`${user.name.first} ${user.name.last}`);
         users.push(user);
     })
@@ -15,15 +16,29 @@ fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
 
 let app = express();
 
+app.set('view engine', 'hbs');
+
+app.use(express.static('images'));
+
 app.get('/', (req, res) => res.send('Hello World!!!!!!!!'));
 app.get('/hello', (req, res) => res.send(`What's up?`));
 app.get('/users', (req, res) => {
-    let buffer = '';
-    users.forEach( user => buffer += `<a href="/${user.username}">${user.name.full}</a><br>` );
-    res.send(buffer);
+
+    //    BEFORE USING TEMPLATE ENGINE FOR RENDERING PAGE CONTENT
+
+    //    let buffer = '';
+    //    users.forEach( user => buffer += `<a href="/${user.username}">${user.name.full}</a><br>` );
+    //    res.send(buffer);
+
+    //    WHEN USING TEMPLATE ENGINE
+
+    res.render('users.hbs', {
+        users: users,
+    })
 });
 app.get('/:username', (req,res) => {
-    let username = req.params.username;
-    res.send(username);
+    res.render('singleUser.hbs', {
+        username: req.params.username,
+    });
 });
 let server = app.listen(3000, () => console.log(`Server running at http://localhost:${server.address().port}`));
